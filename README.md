d2365dbc4883f611a52aa58a510a44486940e6a5
hedhyw <hedhyw@yahoo.com>
2023-06-05 05:39:42 +0300

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# golang-gherkingen README
+
+It is an extension for VS-Code that helps to generate Behaviour Driven Development (BDD) boilerplate Golang tests.
+
+![Animation](./assets/animation.gif)
+
+## Features
+
+- Generate a golang test boilerplate from *.feature Cucumber/Gherkin files by a single click.
+- It uses [this](https://github.com/hedhyw/gherkingen) generator.
+
+## Usage
+
+- Open the command palette (Ctrl+Shift+P or Cmd+Shift+P) and search for "Go: Generate BDD Golang test".
+    ![Command palette](assets/usage-command-palette.png)
+
+- Click the button in the editor's menu:
+    ![Menu editor](assets/usage-editor-menu.png)
+
+## Requirements
+
+- Rootless `docker` is required in order to run [the generator's image](https://hub.docker.com/r/hedhyw/gherkingen).
+
+## Extension Settings
+
+No settings.
+
+## Known Issues
+
+No known issues.
+
+## Release Notes
+
+### 1.0.0
+
+Initial release of golang-gherkingen.
---
+++ src/extension.ts
@@ -0,0 +1,141 @@
+import * as vscode from 'vscode';
+import { promisify } from 'util';
+import { exec, PromiseWithChild } from "child_process";
+
+const dockerImage = 'hedhyw/gherkingen:v3.0.0';
+const execCommand = promisify(exec);
+
+export function activate(context: vscode.ExtensionContext) {
+	let disposableGenerateCommand = vscode.commands.registerCommand(
+		'golang-gherkingen.generate',
+		runGenerateCommand,
+	);
+
+	context.subscriptions.push(disposableGenerateCommand);
+};
+
+function runGenerateCommand(): any {
+	const editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		vscode.window.showInformationMessage('No editor is active.');
+
+		return false;
+	}
+
+	return vscode.window.withProgress({
+		location: vscode.ProgressLocation.Window,
+		cancellable: false,
+		title: 'Generating code...'
+	}, async (progress) => {
+		progress.report({ increment: 0 });
+
+		return editor.document
+			.save()
+			.then((_) => {
+				progress.report({ increment: 50 });
+
+				return pullDockerImage(dockerImage);
+			})
+			.then((_) => {
+				progress.report({ increment: 70 });
+
+				return runDockerGenerator(editor.document.fileName);
+			}).then((output) => {
+				progress.report({ increment: 90 });
+
+				return createGoFile(output.stderr + output.stdout);
+			}).then((document) => {
+				progress.report({ increment: 100 });
+
+				vscode.window.showTextDocument(document, 1, false);
+			});
+	}).then(undefined, (err: any) => {
+		console.error(err);
+
+		return Promise.resolve(false);
+	});
+}
+
+function createGoFile(content: string): Promise<vscode.TextDocument> {
+	return Promise.resolve(vscode.workspace.openTextDocument({
+		language: 'go',
+		content: content,
+	}));
+}
+
+function dockerInspect(image: string): PromiseWithChild<Output> {
+	const command = [
+		'docker',
+		'pull',
+		image,
+	];
+
+	return execCommand(command.join(' '));
+}
+
+function runDockerGenerator(fileName: string): Thenable<Output> {
+	return new Promise<Output>((resolve) => {
+		const command = [
+			'docker',
+			'run',
+			'--rm',
+			'--tty',
+			'--read-only',
+			'--network',
+			'none',
+			'--volume',
+			'"' + fileName + '":"/host/feature":ro',
+			dockerImage,
+			'/host/feature',
+		];
+
+		execCommand(command.join(' ')).then((data) => {
+			console.log("code generated", fileName, data);
+
+			resolve(data);
+		}).catch((err) => {
+			console.error(err);
+
+			resolve({
+				stderr: String(err),
+				stdout: "",
+			});
+		});
+	});
+}
+
+interface Output {
+	stdout: string;
+	stderr: string;
+}
+
+function pullDockerImage(image: string): Thenable<Output> {
+	return new Promise<Output>((resolve) => {
+		dockerInspect(image).then((data) => {
+			console.log("image inspected", image, data);
+
+			resolve(data);
+		}).catch(() => {
+			const command = [
+				'docker',
+				'pull',
+				image,
+			];
+
+			execCommand(command.join(' ')).then((data) => {
+				console.log("image pulled", image, data);
+
+				resolve(data);
+			}).catch((err) => {
+				console.error(err);
+
+				resolve({
+					stderr: String(err),
+					stdout: "",
+				});
+			});
+		});
+	});
+}
+
+export function deactivate() { }
